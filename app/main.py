f9bbc2ca96942cf48774ca607a762fd6ec7ce957
Arian Ahmadinejad <35879206+arian81@users.noreply.github.com>
2023-09-21 11:08:50 -0400

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -9,14 +9,18 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    # server_socket = socket.create_server(("localhost", 4221))
+
     conn, addr = server_socket.accept()  # wait for client
     with conn:
         print(f"connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        conn.recv(1024)
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        # while True:
+        #     data = conn.recv(1024)
+        #     if not data:
+        #         break
+        #     conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
