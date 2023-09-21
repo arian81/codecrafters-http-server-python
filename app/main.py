92a2546cf3950395176043e2e57b5bbafb1ffd31
Arian Ahmadinejad <35879206+arian81@users.noreply.github.com>
2023-09-21 12:15:11 -0400

    pass stage 5

--- app/main.py
+++ app/main.py
@@ -4,16 +4,25 @@ import os
 
 
 def parse(req: bytes):
-    processed = str(req).split("\r\n")
-    path = processed[0].split(" ")[1]
+    processed = req.decode("utf-8").split("\r\n")
+    path = processed.pop(0).split(" ")[1]
+    reqdict = {}
+    for i in processed:
+        if i != "":
+            key, value = i.split(": ")
+            reqdict[key] = value
+    print(reqdict.keys())
+
     if path == "/":
-        return b"HTTP/1.1 200 OK\r\n\r\n"
+        resp = b"HTTP/1.1 200 OK\r\n\r\n"
+    if path == "/user-agent":
+        resp = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(reqdict['User-Agent'])}\r\n\r\n{reqdict['User-Agent']}"
     elif "echo" in path:
         random_string = path[6:]
         resp = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_string)}\r\n\r\n{random_string}"
-        return resp.encode("utf-8")
     else:
-        return b"HTTP/1.1 404 Not Found\r\n\r\n"
+        resp = "HTTP/1.1 404 Not Found\r\n\r\n"
+    return resp.encode("utf-8")
 
 
 def main():
