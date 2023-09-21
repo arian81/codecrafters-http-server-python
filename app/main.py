3127aa746b39938c4e02dffe3249ba85462b77f8
Arian Ahmadinejad <35879206+arian81@users.noreply.github.com>
2023-09-21 13:41:59 -0400

    stage 7 trying to see file directory formatting

--- app/main.py
+++ app/main.py
@@ -2,11 +2,10 @@
 import socket
 import os
 import threading
-import signal
-import sys
+import argparse
 
 
-def parse(req: bytes):
+def parse(req: bytes, directory):
     processed = req.decode("utf-8").split("\r\n")
     path = processed.pop(0).split(" ")[1]
     reqdict = {}
@@ -20,6 +19,15 @@ def parse(req: bytes):
         resp = "HTTP/1.1 200 OK\r\n\r\n"
     elif path == "/user-agent":
         resp = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(reqdict['User-Agent'])}\r\n\r\n{reqdict['User-Agent']}"
+    elif "files" in path:
+        filename = path[7:]
+        if filename in os.listdir(directory):
+            with open(directory + "/" + filename, "rb") as file:
+                data = file.read()
+            resp = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\n\r\n{data}"
+        else:
+            resp = "HTTP/1.1 404 Not Found\r\n\r\n"
+
     elif "echo" in path:
         random_string = path[6:]
         resp = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_string)}\r\n\r\n{random_string}"
@@ -29,16 +37,22 @@ def parse(req: bytes):
     return resp.encode("utf-8")
 
 
-def new_conn(conn: socket, addr):
+def new_conn(conn: socket, addr, directory):
     print(f"connected by {addr}")
     data = conn.recv(1024)
-    conn.sendall(parse(data))
+    conn.sendall(parse(data, directory))
     conn.close()
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
+
+    parser = argparse.ArgumentParser(description="input file directory")
+    parser.add_argument("--directory", type=str)
+    args = parser.parse_args()
+
+    print(args.directory)
 
     # Uncomment this to pass the first stage
     #
@@ -49,7 +63,7 @@ def main():
     threads = []
     while True:
         conn, addr = server_socket.accept()  # wait for client
-        thread = threading.Thread(target=new_conn, args=[conn, addr])
+        thread = threading.Thread(target=new_conn, args=[conn, addr, args.directory])
         thread.start()
         threads.append(thread)
 
