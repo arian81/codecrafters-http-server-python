6b6c05c08427b6592bcd81706c06c66d62c85bd6
Arian Ahmadinejad <35879206+arian81@users.noreply.github.com>
2023-09-21 11:22:02 -0400

    pass stage 3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
 # Uncomment this to pass the first stage
 import socket
+import os
+
+
+def parse(req: bytes):
+    processed = req.split(b"\r\n")
+    print(processed[0].split(b" "))
+    if processed[0].split(b" ")[1] == "/":
+        return b"HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        print("404 sent ")
+        return b"HTTP/1.1 404 Not Found\r\n\r\n"
+    print(processed)
 
 
 def main():
@@ -8,13 +20,16 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket = socket.create_server(("localhost", 4221))
+    if os.name == "nt":
+        server_socket = socket.create_server(("localhost", 4221))
+    else:
+        server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
     conn, addr = server_socket.accept()  # wait for client
     with conn:
         print(f"connected by {addr}")
-        conn.recv(1024)
+        data = conn.recv(1024)
+        parse(data)
         conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
         # while True:
         #     data = conn.recv(1024)
